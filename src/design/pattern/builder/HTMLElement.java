6a3eb9b490bb2887040b7176cb3c6882916aa5b4
Choudhary Shekhar <shekhar.choudhary@tieto.com>
2018-05-12 19:14:14 +0530

    builder pattern added

---
+++ src/design/pattern/builder/HTMLElement.java
@@ -0,0 +1,10 @@
+package design.pattern.builder;
+
+import java.util.ArrayList;
+
+public class HTMLElement {
+
+	public String name, text;
+	public ArrayList<HTMLElement> elements = new ArrayList<>();
+	
+}
